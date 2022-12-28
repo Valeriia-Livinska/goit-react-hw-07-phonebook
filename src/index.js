cd979915838e666ea3bba6005b343a5535fac463
Valeriia-Livinska <valerylivi@gmail.com>
2022-12-28 02:21:54 +0100

    delete persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme';
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
+        {/* <PersistGate loading={null} persistor={persistor}> */}
           <App />
-        </PersistGate>
+        {/* </PersistGate> */}
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
