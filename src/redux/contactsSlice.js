c5d63497e17916177be56bbc2fc424dd6cce30d5
Valeriia-Livinska <valerylivi@gmail.com>
2022-12-28 02:47:49 +0100

    fix

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -11,16 +11,20 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     addContact(state, action) {
-      state.contacts.unshift(action.payload);
+      state.items.unshift(action.payload);
     },
     deleteContact(state, action) {
-      const index = state.contacts.findIndex(
+      const index = state.items.findIndex(
         contact => contact.id === action.payload
       );
-      state.contacts.splice(index, 1);
+      state.items.splice(index, 1);
     },
   },
 });
---
+++ src/redux/operations.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://63ab93b5fdc006ba6060fe38.mockapi.io';
+
+export const fetchContacts = () => { 
+    
+}
